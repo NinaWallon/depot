5fe98a99fa208051ce7d1e59c58d9d6fa16c5266
NinaWallon <nina.wallon@etu.parisdescartes.fr>
2020-03-21 20:03:33 +0000

    .

--- projet.py
+++ projet.py
@@ -20,7 +20,7 @@ if type_vaisseau not in liste_types :
 
 if type_vaisseau == 1 :
 
-  print("Vaisseaux cuirassés : \n 1) Corvette d'Orion \n 2) Talla \n 3) Kumari \n 4) Bortas \n 5) USS Intrepid \n 6) Legionary \n 7) Augur \n")
+  print("\nVaisseaux cuirassés : \n 1) Corvette d'Orion \n 2) Talla \n 3) Kumari \n 4) Bortas \n 5) USS Intrepid \n 6) Legionary \n 7) Augur \n")
 
   modele_vaisseau = int(input("Quel modèle de vaisseau possédez-vous ? \n Tapez un chiffre entre 1 et 7 (voir liste des modèles ci-dessus) : "))
 
@@ -29,10 +29,20 @@ if type_vaisseau == 1 :
   if modele_vaisseau not in liste_cuirasse :
     modele_vaisseau = int(input("Erreur de saisi, veuillez recommencer. \nQuel modèle de vaisseau possédez-vous ? \n Tapez un chiffre entre 1 et 7 (voir liste des modèles ci-dessus) : "))
 
+# Vaisseaux sous forme de dictionnaires avec capacité de chargement (en unités) + vitesse de minage (en sec/unité).
+
+vaisseau_1_1 = {"nom" : "Corvette d'Orion" , "capacite" : 100 , "vitesse" : 1 }
+vaisseau_1_2 = {"nom" : "Talla" , "capacite" : 80 , "vitesse" : 1 }
+vaisseau_1_3 = {"nom" : "Kumari" , "capacite" : 200 , "vitesse" : 1 }
+vaisseau_1_4 = {"nom" : "Bortas" , "capacite" : 230 , "vitesse" : 1 }
+vaisseau_1_5 = {"nom" : "USS Intrepid" , "capacite" : 250 , "vitesse" : 1 }
+vaisseau_1_6 = {"nom" : "Legionary" , "capacite" : 230 , "vitesse" : 1 }
+vaisseau_1_7 = {"nom" : "Augur" , "capacite" : 300 , "vitesse" : 1 }
+
 
 if type_vaisseau == 2 :
 
-  print("Vaisseaux explorateurs : \n 1) Realta \n 2) Turas \n 3) Jellyfish \n 4) USS Franklin \n 5) Vahklas \n 6) Brel \n 7) USS Mayflower \n 8) USS Enterprise \n 9) Centurion \n")
+  print("\nVaisseaux explorateurs : \n 1) Realta \n 2) Turas \n 3) Jellyfish \n 4) USS Franklin \n 5) Vahklas \n 6) Brel \n 7) USS Mayflower \n 8) USS Enterprise \n 9) Centurion \n")
 
   modele_vaisseau = int(input("Quel modèle de vaisseau possédez-vous ? \n Tapez un chiffre entre 1 et 9 (voir liste des modèles ci-dessus) : "))
 
@@ -41,9 +51,22 @@ if type_vaisseau == 2 :
   if modele_vaisseau not in liste_explorateur :
     modele_vaisseau = int(input("Erreur de saisi, veuillez recommencer. \nQuel modèle de vaisseau possédez-vous ? \n Tapez un chiffre entre 1 et 9 (voir liste des modèles ci-dessus) : "))
 
+# Vaisseaux sous forme de dictionnaires avec capacité de chargement (en unités) + vitesse de minage (en sec/unité).
+
+vaisseau_2_1 = {"nom" : "Realta" , "capacite" : 100 , "vitesse" : 1 }
+vaisseau_2_2 = {"nom" : "Turas" , "capacite" : 80 , "vitesse" : 1 }
+vaisseau_2_3 = {"nom" : "Jellyfish" , "capacite" : 100 , "vitesse" : 1 }
+vaisseau_2_4 = {"nom" : "USS Franklin" , "capacite" : 100 , "vitesse" : 1 }
+vaisseau_2_5 = {"nom" : "Vahklas" , "capacite" : 200 , "vitesse" : 1 }
+vaisseau_2_6 = {"nom" : "Brel" , "capacite" : 250 , "vitesse" : 1 }
+vaisseau_2_7 = {"nom" : "USS Mayflower" , "capacite" : 230 , "vitesse" : 1 }
+vaisseau_2_8 = {"nom" : "USS Enterprise" , "capacite" : 300 , "vitesse" : 1 }
+vaisseau_2_9 = {"nom" : "Centurion" , "capacite" : 230 , "vitesse" : 1 }
+
+
 if type_vaisseau == 3 :
 
-  print("Vaisseaux intercepteurs : \n 1) Phindra \n 2) Kehra \n 3) Vi'Dar \n 4) Classe D3 \n 5) Classe D4 \n 6) USS Saladin \n 7) Gladius \n")
+  print("\nVaisseaux intercepteurs : \n 1) Phindra \n 2) Kehra \n 3) Vi'Dar \n 4) Classe D3 \n 5) Classe D4 \n 6) USS Saladin \n 7) Gladius \n")
 
   modele_vaisseau = int(input("Quel modèle de vaisseau possédez-vous ? \n Tapez un chiffre entre 1 et 7 (voir liste des modèles ci-dessus) : "))
 
@@ -52,9 +75,20 @@ if type_vaisseau == 3 :
   if modele_vaisseau not in liste_intercepteur :
     modele_vaisseau = int(input("Erreur de saisi, veuillez recommencer. \nQuel modèle de vaisseau possédez-vous ? \n Tapez un chiffre entre 1 et 7 (voir liste des modèles ci-dessus) : "))
 
+# Vaisseaux sous forme de dictionnaires avec capacité de chargement (en unités) + vitesse de minage (en sec/unité).
+
+vaisseau_3_1 = {"nom" : "Phindra" , "capacite" : 100 , "vitesse" : 1 }
+vaisseau_3_2 = {"nom" : "Kehra" , "capacite" : 80 , "vitesse" : 1 }
+vaisseau_3_3 = {"nom" : "Vi'Dar" , "capacite" : 100 , "vitesse" : 1 }
+vaisseau_3_4 = {"nom" : "Classe D3" , "capacite" : 100 , "vitesse" : 1 }
+vaisseau_3_5 = {"nom" : "Classe D4" , "capacite" : 200 , "vitesse" : 1 }
+vaisseau_3_6 = {"nom" : "USS Saladin" , "capacite" : 250 , "vitesse" : 1 }
+vaisseau_3_7 = {"nom" : "Gladius" , "capacite" : 230 , "vitesse" : 1 }
+
+
 if type_vaisseau == 4 :
 
-  print("Vaisseaux de prospection : \n 1) ECS Fortunate \n 2) Envoy \n 3) Botany Bay \n 4) NOrth Star \n 5) D'Vor Frengi \n 6) ECS Horizon \n 7) USS Antares \n 8) Valkis \n")
+  print("\nVaisseaux de prospection : \n 1) ECS Fortunate \n 2) Envoy \n 3) Botany Bay \n 4) North Star \n 5) D'Vor Ferengi \n 6) ECS Horizon \n 7) USS Antares \n 8) Valkis \n")
 
   modele_vaisseau = int(input("Quel modèle de vaisseau possédez-vous ? \n Tapez un chiffre entre 1 et 8 (voir liste des modèles ci-dessus) : "))
 
@@ -63,6 +97,18 @@ if type_vaisseau == 4 :
   if modele_vaisseau not in liste_prospection :
     modele_vaisseau = int(input("Erreur de saisi, veuillez recommencer. \nQuel modèle de vaisseau possédez-vous ? \n Tapez un chiffre entre 1 et 8 (voir liste des modèles ci-dessus) : "))
 
+# Vaisseaux sous forme de dictionnaires avec capacité de chargement (en unités) + vitesse de minage (en sec/unité).
+
+vaisseau_4_1 = {"nom" : "ECS Fortunate" , "capacite" : 200 , "vitesse" : 2 }
+vaisseau_4_2 = {"nom" : "Envoy" , "capacite" : 900 , "vitesse" : 3 }
+vaisseau_4_3 = {"nom" : "Botany Bay" , "capacite" : 1000 , "vitesse" : 3 }
+vaisseau_4_4 = {"nom" : "North Star" , "capacite" : 300 , "vitesse" : 6 }
+vaisseau_4_5 = {"nom" : "D'Vor Ferengi" , "capacite" : 3955 , "vitesse" : 2 }
+vaisseau_4_6 = {"nom" : "ECS Horizon" , "capacite" : 1750 , "vitesse" : 8 }
+vaisseau_4_7 = {"nom" : "USS Antares" , "capacite" : 3000 , "vitesse" : 9 }
+vaisseau_4_8 = {"nom" : "Valkis" , "capacite" : 3000 , "vitesse" : 9 }
+
+
 
 # 3 - Demander à l'utilisateur quelle ressource miner.
 # Parsteel (pour la construction de bâtiments de station), tritanium (pour la construction de coques et cloisons de vaisseaux),
@@ -70,6 +116,31 @@ if type_vaisseau == 4 :
 # cristal (pour améliorer les intercepteurs), minerai (pour améliorer les cuirassés)
 # ou latinium (pour la construction et l'assemblage de vaisseaux).
 
+print("\nListe de ressources à miner : \n 1) Parsteel (pour la construction de bâtiments de station) \n 2) Tritanium (pour la construction de coques et cloisons de vaisseaux) \n 3) Dilithium (pour la contruction de bâtiments et de vaisseaux) \n 4) Minerai (pour améliorer les cuirassés) \n 5) Gaz (pour améliorer les explorateurs) \n 6) Cristal (pour améliorer les intercepteurs) \n 7) Latinium (pour la construction et l'assemblage de vaisseaux) \n")
+
+ressource = int(input("Quelle ressource voulez-vous miner ? \n Tapez un chiffre en 1 et 7 (voir liste des ressources ci-dessus) : "))
+
+liste_ressources = [1,2,3,4,5,6,7]
+
+if ressource not in liste_ressources :
+  ressource = int(input("Erreur de saisi, veuillez recommencer. \nQuelle ressource voulez-vous miner ? \n Tapez un chiffre en 1 et 7 (voir liste des ressources ci-dessus) : "))
+
+
+# Coefficient des ressources
+
+res_1 = {"nom" : "Parsteel" , "coef" : 1}
+res_2 = {"nom" : "Tritanium" , "coef" : 4}
+res_3 = {"nom" : "Dilithium" , "coef" : 15}
+res_4 = {"nom" : "Minerai" , "coef" : 5}
+res_5 = {"nom" : "Gaz" , "coef" : 5}
+res_6 = {"nom" : "Cristal" , "coef" : 5}
+res_7 = {"nom" : "Latinium" , "coef" : 50}
+
+
 # 4 - Calcul du temps de minage en fonction de la capacité de chargement et de la vitesse de minage du vaisseau
 # (le temps de chargement diffère en fonction de la ressource à miner).
 # Convertir les secondes en minutes voire en heures (+ lisible).
+
+temps = vaisseau_type_vaisseau_modele_vaisseau["capacite"]*vaisseau_type_vaisseau_modele_vaisseau["vitesse"]*res_ressource["coef"]
+
+print("\nVotre vaisseau aura fini de miner dans ", temps, " secondes.")
