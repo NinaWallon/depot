60217ce9dae053a9fbd190620f6e8dc3cdf0973c
NinaWallon <nina.wallon@etu.parisdescartes.fr>
2020-03-21 17:58:54 +0000

    .

--- projet.py
+++ projet.py
@@ -1,1 +1,15 @@
-# Trouver une idée de projet
+# Idée de projet : En combien de temps mon vaisseau spatiale aura-t-il fini de miner ? (Inspiration Star Trek Fleet Command)
+
+# 1 - Demander à l'utilisateur le type de vaisseau.
+# Cuirassé, Explorateur, Intercepteur ou Prospection (conseillé)
+
+# 2 - Demander à l'utilisateur le nom du vaisseau.
+# Pourquoi ne pas demander directement ?
+# Parce que sinon la liste de vaisseaux est trop longue (pas très lisible)
+
+# 3 - Demander à l'utilisateur quel élément miner.
+# Parsteel, tritanium, dilithium, gaz, cristal, minerai ou lithium
+
+# 4 - Calcul du temps de minage en fonction de la capacité de chargement et de la vitesse de minage du vaisseau (le temps de chargement diffère en fonction de l'élément à miner).
+# Convertir les secondes en minutes voire en heures (+ lisible).
+
